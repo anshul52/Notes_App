0b1ba021d5347b2eea91f4b163b751c26b59737b
AnshulGupta <anshulgupta2028@gmail.com>
2024-11-21 01:51:38 +0530

    new push

--- config/db.js
+++ config/db.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 const uri = process.env.MONGODB_URI;
-// ("mongodb+srv://anshulgupta2028:n8A8CdHV7PA4TPYx@notesappcluster.ppx8t.mongodb.net/?retryWrites=true&w=majority&appName=NotesAppCluster");
 
 async function dbConnect() {
   try {
